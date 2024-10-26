3849d28bd923fdc7112d4db2b6d7df6ef98fb429
Caner Çin <canercincin34@gmail.com>
2024-10-26 18:16:32 +0300

    Create Adder1.v

---
+++ Adder1.v
@@ -0,0 +1,15 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+  
+    wire first_cout, second_cout;
+    wire [15:0] first_wire, second_wire;
+    
+    add16 first(a[15:0], b[15:0], 0, first_wire, first_cout);
+    add16 second(a[31:16], b[31:16], first_cout, second_wire, second_cout);
+    
+    assign sum = {second_wire, first_wire};
+  
+endmodule
