d1eb9df376d549a0465e4dc79187d35037bdda7c
canercin <canercincin34@gmail.com>
2024-12-22 20:27:49 +0300

    some FSM answers added

---
+++ circuits/sequential_logic/finite_state_machines/simple_fsm_2_sync_reset.v
@@ -0,0 +1,32 @@
+module top_module(
+    input clk,
+    input reset,    // Synchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        case (state)
+        	ON: next_state = k ? OFF : ON;
+            OFF: next_state = j ? ON : OFF;
+        endcase
+    end
+
+    always @(posedge clk) begin
+        // State flip-flops with synchronous reset
+        if (reset) begin
+        	state <= OFF;
+        end else begin
+        	state <= next_state;
+        end
+    end
+
+    // Output logic
+    // assign out = (state == ...);
+    assign out = (state == OFF) ? 0 : 1;
+
+endmodule
+
